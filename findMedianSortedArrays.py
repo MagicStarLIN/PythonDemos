69b48a63e2eb7cf0e6c4a3a043e905559afd1065
刘常林 <751969316@qq.com>
2019-12-25 16:07:45 +0800

    注释

--- findMedianSortedArrays.py
+++ findMedianSortedArrays.py
@@ -1,3 +1,21 @@
+# 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+# 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+# 你可以假设 nums1 和 nums2 不会同时为空。
+# 示例 1:
+# nums1 = [1, 3]
+# nums2 = [2]
+# 则中位数是 2.0
+# 示例 2:
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+# 则中位数是 (2 + 3)/2 = 2.5
+
+
+# 执行结果：通过
+# 执行用时 :92 ms, 在所有 python3 提交中击败了99.61%的用户
+# 内存消耗 :12.8 MB, 在所有 python3 提交中击败了99.43%的用户
+
+
 def findKthElm(nums1,nums2,k):
     min_int = -9223372036854775807
     assert (1 <= k and k <= len(nums1) + len(nums2))
@@ -22,18 +40,3 @@ def findMedianSortedArrays (nums1 , nums2) :
         return float(findKthElm(nums1,nums2,(n>>1)+1))
     else:
         return float((findKthElm(nums1,nums2,n>>1)+findKthElm(nums1,nums2,(n>>1)+1))/2)
-    
-
-nums1 = [1,2]
-nums2 = [3,4]
-
-print(findMedianSortedArrays(nums1,nums2))
-
-#TypeError: None is not valid value for the expected return type double
-# Line 54 in _driver (Solution.py)
-# Line 63 in <module> (Solution.py)
-# During handling of the above exception, another exception occurred:
-# TypeError: must be real number, not NoneType
-# Line 18 in _serialize_float (./python3/__serializer__.py)
-# Line 59 in _serialize (./python3/__serializer__.py)
-# Line 52 in _driver (Solution.py)
